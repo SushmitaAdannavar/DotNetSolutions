0d41c6b57096eb970d89e368801aba23dd3c3e37
SushmitaAdannavar <131010883+SushmitaAdannavar@users.noreply.github.com>
2023-07-18 18:43:31 +0530

    Uploaded Folder2

--- Program.cs
+++ Program.cs
@@ -1,33 +1,14 @@
 ﻿using System;
-using System.Reflection.PortableExecutable;
 
-namespace ConsoleApp3
+namespace ConsoleApp2
 {
-    class Program
+    public class Program
     {
-        
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            /*rectangle rect1 = new rectangle();
-            rectangle rect2 = new rectangle(4,6);
-            rectangle rect4 = new rectangle(rect2);*/
-            /*Iaddition a;
-            a = new add1();
-            a.add();
-            a = new add2();
-            a.add();
-            accessmodifiers am = new accessmodifiers();
-            am.a = 10;
-            protectedaccessmodifier pm = new protectedaccessmodifier();
-            pm.b = 10;*/
-            //weekday.dayname();
-            // power.powerof2();
-            /* sum sm = new sum();
-             sm.sumof();*/
-            /*string1 str = new string1();
-            str.strrev();*/
-            Flipcoin fp = new Flipcoin();
-            fp.headstails();
+            problem.integerequal();
+            problem.votecast();
+            problem.greatestnum();
 
         }
     }
